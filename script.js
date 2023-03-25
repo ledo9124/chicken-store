ca25d758be0c25d8d2bfd486c75bdb965a262cde
ledo9124 <leminhdo904@gmail.com>
2023-03-25 19:29:25 +0700

    finish cart

--- script.js
+++ script.js
@@ -17,3 +17,37 @@ tabsElement.forEach(element => {
     }
 });
 
+const itemsElement = document.querySelectorAll('.list-products .item');
+
+itemsElement.forEach(item => {
+    item.addEventListener('click' , function(event) {
+        if (event.target.classList.contains('add-count')) {
+            let itemNew = item.cloneNode(true);
+            let checkIsset = false;
+            let listCart = document.querySelectorAll('.cart .item');
+            listCart.forEach(cart => {
+                if (cart.getAttribute('data-key') === itemNew.getAttribute('data-key')){
+                    checkIsset = true;
+                    cart.classList.add('danger');
+                    setTimeout(function() {
+                        cart.classList.remove('danger');
+                    },1000)
+                }
+            })
+            if (checkIsset === false) {
+                document.querySelector('.listCart').appendChild(itemNew);
+            }
+        }
+    })
+})
+
+function remove(key) {
+    let listCart = document.querySelectorAll('.cart .item');
+    listCart.forEach(item => {
+        if(item.getAttribute('data-key') == key){
+            item.remove();
+            return;
+        }
+    })
+}
+
